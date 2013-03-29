cbb2d8f615fde03dce9ce3107fe96026d63addc1
DimkOf <dmitry@dimkof.com>
2013-03-29 18:02:58 +0200

    Added driver of class Config

---
+++ framework/Core/Config/Sqlite.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core\Config;
+
+class Database extends \Core\Config {
+
+	/**
+	 * Load config group
+	 *
+	 * @param string $config_group
+	 * @return array configs
+	 */
+	public function load($config_group)
+	{
+		return $this;
+	}
+
+	/**
+	 * Get config key
+	 *
+	 * @param string $config_key
+	 * @return mixed
+	 */
+	public function get($config_key)
+	{
+		return $config_key;
+	}
+
+	/**
+	 * Set to config
+	 *
+	 * @param string $config_key
+	 * @param mixed $data
+	 * @return boolean
+	 */
+	public function set($config_key, $data)
+	{
+		return TRUE;
+	}
+}
