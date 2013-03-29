9df3ae1a92798190a809a883c4addfea42cd7c47
DimkOf <dmitry@dimkof.com>
2013-03-29 18:01:13 +0200

    class in developing

--- framework/Core/Config.php
+++ framework/Core/Config.php
@@ -2,6 +2,55 @@
 
 namespace Core;
 
-class Config {
+abstract class Config {
 
+	/**
+	 * Load config group
+	 *
+	 * @param string $config_group
+	 * @return array configs
+	 */
+	abstract public function load($config_group);
+
+	/**
+	 * Get config key
+	 *
+	 * @param string $config_key
+	 * @return mixed
+	 */
+	abstract public function get($config_key);
+
+	/**
+	 * Set to config
+	 *
+	 * @param string $config_key
+	 * @param mixed $data
+	 * @return boolean
+	 */
+	abstract public function set($config_key, $data);
+
+    /**
+     * @var array class instance
+     */
+    private static $_instance = array();
+
+    /**
+     * @param array $data
+     * @return class instance
+     */
+    public static function instance($instance = 'file')
+    {
+        if(
+            ! array_key_exists($instance, self::$_instance)
+            OR ! (self::$_instance[$instance] instanceof self)
+        )
+        {
+			$config_class = \Helpers\UTF8::ucfirst($instance);
+			$config_class = 'Core\\Config\\'.$config_class;
+
+            self::$_instance[$instance] = new $config_class;
+        }
+
+        return self::$_instance[$instance];
+    }
 }
--- framework/Core/Simply.php
+++ framework/Core/Simply.php
@@ -29,24 +29,20 @@ class Simply {
 	const DEVELOPMENT = 40;
 
 	/**
-	 * Array paths for autoload
-	 *
-	 * @void array
+	 * @void boolean
 	 */
-	public static $_paths = array(SYSPATH);
+	public static $_init = FALSE;
 
 	/**
 	 * Initialization framework.
 	 * Set Autoload.
 	 * Sanitize request valiable.
+	 * Disables register_globals and magic_quotes_gpc.
+	 *
+	 * @return  void
 	 */
 	public static function init()
 	{
-		// Sanitize all request variables
-		$_GET    = Security::sanitize($_GET);
-		$_POST   = Security::sanitize($_POST);
-		$_COOKIE = Security::sanitize($_COOKIE);
-
 		// Enable the Simply exception handling, adds stack traces and error source.
 		// set_exception_handler(array('Exception', 'exception_handler'));
 
@@ -55,13 +51,205 @@ class Simply {
 
 		// Enable the Simply shutdown handler, which catches E_FATAL errors.
 		// register_shutdown_function(array('Exception', 'shutdown_handler'));
+
+		if (ini_get('register_globals'))
+		{
+			// Reverse the effects of register_globals
+			self::globals();
+		}
+
+		// Sanitize all request variables
+		$_GET    = self::sanitize($_GET);
+		$_POST   = self::sanitize($_POST);
+		$_COOKIE = self::sanitize($_COOKIE);
+
+		self::$_init = TRUE;
 	}
 
+	/**
+	 * @return  void
+	 */
+	public static function deinit()
+	{
+		if (self::$_init)
+		{
+			// Removed the autoloader
+			spl_autoload_unregister(array('\Core\Simply', 'auto_load'));
 
+			// Go back to the previous error handler
+			restore_error_handler();
+
+			// Go back to the previous exception handler
+			restore_exception_handler();
+
+			// Kohana is no longer initialized
+			self::$_init = FALSE;
+		}
+	}
+
+	/**
+	 * Autoload classes
+	 *
+	 * @retrun boolean
+	 */
 	public static function auto_load($class_name)
 	{
 		return Autoload::get_class($class_name);
 	}
+
+	/**
+	 * Cache
+	 *
+	 * @return class instance
+	 */
+	public static function cache($cache_type = 'file')
+	{
+		return Cache::instance($cache_type);
+	}
+
+	/**
+	 * Config
+	 *
+	 * @return class instance
+	 */
+	public static function config($driver = 'file')
+	{
+		return Config::instance($driver);
+	}
+
+	/**
+	 * Cookies
+	 *
+	 * @return class instance
+	 */
+	public static function cookie()
+	{
+
+	}
+
+	/**
+	 * Event
+	 *
+	 * @return class instance
+	 */
+	public static function event()
+	{
+
+	}
+
+	/**
+	 * Searches for a file in the Cascading Filesystem,
+	 * and returns the path to the file that has the highest precedence,
+	 * so that it can be included.
+	 *
+	 * @return class instance
+	 */
+	public static function find_file($dir, $file, $ext = NULL, $array = FALSE)
+	{
+		return Autoload::find_file($dir, $file, $ext, $array);
+	}
+
+	/**
+	 * Reverts the effects of the `register_globals` PHP setting by unsetting
+	 * all global varibles except for the default super globals (GPCS, etc),
+	 * which is a [potential security hole.][ref-wikibooks]	 *
+	 *
+	 * [ref-wikibooks]: http://en.wikibooks.org/wiki/PHP_Programming/Register_Globals
+	 *
+	 * @return  void
+	 */
+	public static function globals()
+	{
+		return Security::globals();
+	}
+
+	/**
+	 * Logs
+	 *
+	 * @return class instance
+	 */
+	public static function log()
+	{
+
+	}
+
+	/**
+	 * Message
+	 *
+	 * @return class instance
+	 */
+	public static function message()
+	{
+
+	}
+
+	/**
+	 * Registry
+	 *
+	 * @return class instance
+	 */
+	public static function registry($instance = 'default', $data = array())
+	{
+		return Registry::instance($instance, $data);
+	}
+
+	/**
+	 * Request
+	 *
+	 * @return class instance
+	 */
+	public static function request()
+	{
+
+	}
+
+	/**
+	 * Response
+	 *
+	 * @return class instance
+	 */
+	public static function response()
+	{
+
+	}
+
+	/**
+	 * Sanitize data
+	 *
+	 * @return mixed
+	 */
+	public static function sanitize($data)
+	{
+		return Security::sanitize($data);
+	}
+
+	/**
+	 * Session
+	 *
+	 * @return class instance
+	 */
+	public static function session()
+	{
+		return Session::instance();
+	}
+
+	/**
+	 * Magic method to get an class instance of the namespace Core
+	 *
+	 * @return class instance
+	 */
+	public static function __callStatic($class, $arguments)
+	{
+		$class = \Helpers\UTF8::ucfirst($class);
+		$class = '\\Core\\'.$class;
+
+		if (class_exists($class))
+		{
+			return new $class($arguments);
+		}
+
+		echo 'Class '.$class.' not exists';
+    }
 
 	/**
 	 * Simply Framework Copyright
