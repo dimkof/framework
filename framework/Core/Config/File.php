3cf446a4fc8235dc5929057337c751d12c883c7a
DimkOf <dmitry@dimkof.com>
2013-03-29 17:58:31 +0200

    added drivers of class Config

--- framework/Core/Config/File.php
+++ framework/Core/Config/File.php
@@ -2,6 +2,39 @@
 
 namespace Core\Config;
 
-class File {
+class File extends \Core\Config {
 
+	/**
+	 * Load config group
+	 *
+	 * @param string $config_group
+	 * @return array configs
+	 */
+	public function load($config_group)
+	{
+		return $this;
+	}
+
+	/**
+	 * Get config key
+	 *
+	 * @param string $config_key
+	 * @return mixed
+	 */
+	public function get($config_key)
+	{
+		return $config_key;
+	}
+
+	/**
+	 * Set to config
+	 *
+	 * @param string $config_key
+	 * @param mixed $data
+	 * @return boolean
+	 */
+	public function set($config_key, $data)
+	{
+		return TRUE;
+	}
 }
