57daa9d519821a907eab6cd8c75d2231ef314d20
DimkOf <dmitry@dimkof.com>
2013-04-29 07:40:02 +0300

    tests

--- index.php
+++ index.php
@@ -6,3 +6,6 @@ require_once(DOCROOT.'framework/bootstrap.php');
 echo \Core\Debug::time(START_TIME).'<br>';
 echo \Core\Debug::memory(START_MEMORY).'<br>';
 echo \Core\Debug::time(START_TIME, 'sec').'<br>';
+date_default_timezone_set('America/Chicago');
+
+echo date_default_timezone_get();
